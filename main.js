ce02b4ddbd9a9e3a5e026020ff41568baa1efd97
Timewound <rubenjms1@gmail.com>
2023-08-03 10:53:33 -0300

    mudando var para const

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var buttons = document.querySelectorAll('.tecla');
-var songs = document.querySelectorAll('audio');
+const buttons = document.querySelectorAll('.tecla');
+const songs = document.querySelectorAll('audio');
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function(){songs[i].play()});
