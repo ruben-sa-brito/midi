81f279a2bfd8ce8343f62bf3fb7a11d2c9b31e54
Timewound <rubenjms1@gmail.com>
2023-08-03 10:22:02 -0300

    adicionando som ao clicar em cada botao

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-document.querySelector('.tecla_pom');
+var buttons = document.querySelectorAll('.tecla');
+var songs = document.querySelectorAll('audio');
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(){songs[i].play()});
+
+  };
