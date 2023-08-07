1fe4b3102dc1b2ada9258515c9b84b58f9edeb0e
Timewound <rubenjms1@gmail.com>
2023-08-07 10:19:01 -0300

    ajustando tecla enter e space para reproduzir som

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ const buttons = document.querySelectorAll('.tecla');
 const songs = document.querySelectorAll('audio');
 
 for (let i = 0; i < buttons.length; i++) {
+
     buttons[i].addEventListener('click', function(){songs[i].play()});
+
+    buttons[i].onkeydown = function (evento){
+      if (evento.code === 'Space' || evento.code === 'Enter'){
+        buttons[i].classList.add('ativa');
+      }
+    }
+
+    buttons[i].onkeyup = function (){
+      buttons[i].classList.remove('ativa');
+
+    }
 
   };
